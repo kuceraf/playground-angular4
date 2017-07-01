05caa2f9b549c90c8c34d24cd53427b38867faaf
Filip Kucera <kucerafilip@gmail.com>
2017-07-01 17:27:28 +0200

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPageComponent,
+    HomePageComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
